36e82514fea86d416235fc8c5c2d91e4cfe07087
Ema Forlin <emaa.forlin@gmail.com>
2025-02-21 00:27:41 -0300

    Add config package

---
+++ config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"flag"
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	App App
+	DB  DB
+}
+
+type App struct {
+	Port uint16
+}
+
+type DB struct {
+	DBName   string
+	Port     uint16
+	Host     string
+	User     string
+	Password string
+	Migrate  *bool
+}
+
+var config *Config
+
+func Init() {
+	viper.SetConfigType("yaml")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".config/")
+	viper.AddConfigPath("./")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("error parsing default config file")
+	}
+
+	config = &Config{
+		App: App{
+			viper.GetUint16("service.port"),
+		},
+		DB: DB{
+			DBName:   viper.GetString("database.dbname"),
+			Host:     viper.GetString("database.host"),
+			Port:     viper.GetUint16("database.port"),
+			User:     viper.GetString("database.user"),
+			Password: viper.GetString("database.password"),
+			Migrate:  flag.Bool("migrate", false, "set to true to migrate database schema"),
+		},
+	}
+	flag.Parse()
+}
+
+func GetConfig() *Config {
+	return config
+}
