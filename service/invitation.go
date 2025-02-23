861b1ec0f04f1642efbeb3738f70843629f8321d
Ema Forlin <emaa.forlin@gmail.com>
2025-02-23 17:17:07 -0300

    Implement service

---
+++ service/invitation.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/hex"
+	"time"
+
+	"github.com/emaforlin/bussiness-service/entities"
+)
+
+const tokenBytesLength = 16
+
+// SendJoinInvitation implements Service.
+func (s *service) SendJoinInvitation(ctx context.Context, invitation entities.InvitationDto) (string, error) {
+	token, _ := generateToken(tokenBytesLength)
+
+	err := s.db.Cursor().Create(&entities.Invitation{
+		InviterID:      invitation.InviterID,
+		Token:          token,
+		RecipientEmail: invitation.RecipientEmail,
+		AssignedRole:   invitation.AssignedRole,
+		Expiration:     time.Now().Add(3 * 24 * time.Hour).Unix(),
+	}).Error
+
+	if err != nil {
+		return "", err
+	}
+
+	s.logger.Debug("Invitation sent")
+	return token, nil
+}
+
+func generateToken(length int) (string, error) {
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
